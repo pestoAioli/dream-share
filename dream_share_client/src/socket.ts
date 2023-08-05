4e5390dcb58bf021768fef19588bc75b9f795d5b
Ricardo Rivera <ricardorivera@Ricardos-MacBook-Pro.local>
2023-08-04 22:59:40 -0700

    vite is annoying

--- dream_share_client/src/socket.ts
+++ dream_share_client/src/socket.ts
@@ -2,7 +2,7 @@ import { Socket } from "phoenix";
 
 export default function socket(topic: string) {
   //@ts-ignore
-  let socket = new Socket(import.meta.env.SOCKET_URL, { params: { token: 1 } })
+  let socket = new Socket(import.meta.env.VITE_SOCKET_URL, { params: { token: 1 } })
   socket.connect();
   let channel = socket.channel(topic, {});
   channel
