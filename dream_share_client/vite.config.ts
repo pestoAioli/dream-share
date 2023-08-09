d2c81895155570c298e0992e3b6526a0921095bd
Ricardo Rivera <ricardorivera@Ricardos-MacBook-Pro.local>
2023-08-08 17:42:10 -0700

    update vite config for pwa

--- dream_share_client/vite.config.ts
+++ dream_share_client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
@@ -10,6 +11,40 @@ export default defineConfig({
     */
     // devtools(),
     solidPlugin(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
+      manifest: {
+        name: 'dreaming of u',
+        short_name: '😴💤',
+        start_url: 'https://dreaming-of-u.com/dreamfeed',
+        description: 'Save and share your dreams with other ppl :p',
+        theme_color: '#FFFFFF',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: "/mask-icon.png",
+            sizes: "383x383",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+        ]
+      }
+    })
   ],
   server: {
     port: 3000,
